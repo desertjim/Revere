97400ee0ce229f9372663ef10ca4a9dd99ec3460
James Baca <james@jamesbaca.net>
2015-07-13 05:39:47 -0700

    Adding progress callback interface

---
+++ app/src/main/java/bluetooth/jamesbaca/net/bluetoothextensions/Progress.java
@@ -0,0 +1,10 @@
+package bluetooth.jamesbaca.net.bluetoothextensions;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+
+public interface Progress {
+    void onProgress(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status, byte[] value);
+    void onError(Throwable e);
+    void onComplete();
+}
