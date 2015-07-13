ab58309d64d9b1505b3141fa191931c27c831f87
James Baca <james@jamesbaca.net>
2015-07-13 05:40:48 -0700

    Adding in the reading of a byte array

---
+++ app/src/main/java/bluetooth/jamesbaca/net/bluetoothextensions/CharacteristicReadByteArray.java
@@ -0,0 +1,57 @@
+package bluetooth.jamesbaca.net.bluetoothextensions;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class CharacteristicReadByteArray extends BluetoothGattCallback {
+
+    public static final int DEFAULT_BTLE_MAX_BYTE_ARRAY_SIZE = 20;
+    byte[] mMAXByteChunkToWrite;
+    ReadSplitter mSplitter;
+    Progress mProgress;
+
+    public CharacteristicReadByteArray() {
+
+    }
+
+    @Override
+    public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+        if (status == BluetoothGatt.GATT_SUCCESS) {
+            storeChunk(gatt, characteristic);
+            mProgress.onProgress(gatt, characteristic, status, mMAXByteChunkToWrite);
+            readChunk(gatt, characteristic);
+        } else {
+            mProgress.onError(new RuntimeException("Characteristic Read Failed: " + status));
+        }
+    }
+
+    public void start(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, Progress progress) {
+        mProgress = progress;
+        readChunk(gatt, characteristic);
+    }
+
+    private void readChunk(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+        if (mSplitter.hasNext()) {
+            gatt.readCharacteristic(characteristic);
+        } else {
+            mProgress.onComplete();
+        }
+    }
+
+    private void storeChunk(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+        mMAXByteChunkToWrite = characteristic.getValue();
+
+        try {
+
+            mSplitter.addNext(mMAXByteChunkToWrite);
+
+        } catch (Exception e) {
+            mProgress.onError(e);
+        }
+    }
+}
