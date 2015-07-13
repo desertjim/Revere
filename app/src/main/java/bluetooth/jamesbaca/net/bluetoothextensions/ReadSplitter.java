b256a3a3c4eb3f88c81b85394955b7db6439dba6
James Baca <james@jamesbaca.net>
2015-07-13 05:40:11 -0700

    Adding in a read splitter

---
+++ app/src/main/java/bluetooth/jamesbaca/net/bluetoothextensions/ReadSplitter.java
@@ -0,0 +1,44 @@
+package bluetooth.jamesbaca.net.bluetoothextensions;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class ReadSplitter implements Iterator<byte[]> {
+
+    int mSplitByteSize;
+    int mLastByteSizeRead;
+    ByteArrayOutputStream mByteArrayOutputStream = new ByteArrayOutputStream();
+
+    public ReadSplitter(int splitByteSize) {
+        mSplitByteSize = splitByteSize;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return mLastByteSizeRead == mSplitByteSize;
+    }
+
+    @Override
+    public byte[] next() {
+        return null;
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    public void addNext(byte[] contents){
+        try {
+            if (contents != null && contents.length > 0) {
+                mByteArrayOutputStream.write(contents);
+            }
+            mLastByteSizeRead = contents.length;
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
