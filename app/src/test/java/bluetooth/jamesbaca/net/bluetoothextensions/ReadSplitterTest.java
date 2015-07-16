d1781beb67539f8240c4cf6a35add6923bc2be7b
James Baca <james@jamesbaca.net>
2015-07-16 05:24:26 -0700

    Adding in ReadSplitterTest

---
+++ app/src/test/java/bluetooth/jamesbaca/net/bluetoothextensions/ReadSplitterTest.java
@@ -0,0 +1,35 @@
+package bluetooth.jamesbaca.net.bluetoothextensions;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+
+public class ReadSplitterTest {
+
+    @Test
+    public void testHasNext() {
+        int readSize = 20;
+        byte []contents1 = new byte[20];
+        ReadSplitter splitter = new ReadSplitter(readSize);
+        splitter.addNext(contents1);
+        assertThat("Should indicate that more is expected", splitter.hasNext(), is(true));
+    }
+
+    @Test
+    public void testHasNextAfterWrite0() {
+        int readSize = 20;
+        byte []contents1 = new byte[20];
+        byte []emptyContents = new byte[0];
+        ReadSplitter splitter = new ReadSplitter(readSize);
+        splitter.addNext(contents1);
+        splitter.addNext(emptyContents);
+        assertThat("Should indicate nothing more is expected", splitter.hasNext(), is(false));
+    }
+
+
+}
