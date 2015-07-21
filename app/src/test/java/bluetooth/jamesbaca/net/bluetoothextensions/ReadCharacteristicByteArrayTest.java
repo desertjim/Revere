ac5f6fb7260c01949421bb07eb668e8c6ee000bb
James Baca <james@jamesbaca.net>
2015-07-21 06:16:51 -0700

    Stub for first read characteristic test

--- app/src/test/java/bluetooth/jamesbaca/net/bluetoothextensions/ReadCharacteristicByteArrayTest.java
+++ app/src/test/java/bluetooth/jamesbaca/net/bluetoothextensions/ReadCharacteristicByteArrayTest.java
@@ -1,4 +1,19 @@
 package bluetooth.jamesbaca.net.bluetoothextensions;
 
+import android.annotation.TargetApi;
+import android.os.Build;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class ReadCharacteristicByteArrayTest {
+
+    @Test
+    public void ReadCharacteristicByteArrayTest(){
+
+    }
+
 }
