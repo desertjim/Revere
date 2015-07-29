578d29a7f65fd23d440d9a7cbbb48094ee400f74
James Baca <james@jamesbaca.net>
2015-07-29 16:54:55 -0700

    New filenames

---
+++ app/src/test/java/bluetooth/jamesbaca/net/bluetoothextensions/BluetoothPluggableTest.java
@@ -0,0 +1,31 @@
+package bluetooth.jamesbaca.net.bluetoothextensions;
+
+import android.annotation.TargetApi;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.os.Build;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.robolectric.RobolectricTestRunner;
+
+import bluetooth.jamesbaca.net.bluetoothextensions.callbacks.CharacteristicReadByteArray;
+import bluetooth.jamesbaca.net.bluetoothextensions.callbacks.Progress;
+import bluetooth.jamesbaca.net.bluetoothextensions.helpers.ReadSplitter;
+
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+
+@RunWith(RobolectricTestRunner.class)
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+public class BluetoothPluggableTest {
+
+    @Test
+    public void BluetoothPluggableTest(){
+
+    }
+
+}
