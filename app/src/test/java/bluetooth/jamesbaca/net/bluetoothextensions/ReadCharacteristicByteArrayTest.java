1262f96a94f1716d12692dcc75365e12b8884fd5
James Baca <james@jamesbaca.net>
2015-07-31 22:03:26 -0700

    Adding in @Config Annotation

--- app/src/test/java/bluetooth/jamesbaca/net/bluetoothextensions/ReadCharacteristicByteArrayTest.java
+++ app/src/test/java/bluetooth/jamesbaca/net/bluetoothextensions/ReadCharacteristicByteArrayTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import java.util.Arrays;
 
@@ -26,6 +27,7 @@ import static org.mockito.Mockito.mock;
 
 @RunWith(RobolectricTestRunner.class)
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+@Config(manifest= Config.NONE)
 public class ReadCharacteristicByteArrayTest {
 
     @Test
